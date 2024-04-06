e5ef27045f4d5ee059e33890e00522f2ea36fef3
Victor <chestersmail@rambler.ru>
2024-04-06 20:53:58 +0300

    homework for lesson_3 in module_2 added v1

--- module_2/lesson_3/src/solution_1.py
+++ module_2/lesson_3/src/solution_1.py
@@ -1,9 +1,11 @@
 products = [1, 14, 4, 8, 6, 9]
 
-def wtf(incoming):
-    sum = 0
-    for i in incoming:
-        sum += i
-    return sum
+# def wtf(incoming):
+#     sum = 0
+#     for i in incoming:
+#         sum += i
+#     return sum
 
-print(wtf(products))
+# print(wtf(products))
+
+print(sum(products))
