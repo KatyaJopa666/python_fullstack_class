91be3144c818ed0d6410796433312f529ad6c167
Victor <chestersmail@rambler.ru>
2024-06-22 02:10:39 +0300

    solution_4.py in module_2, lesson_13 corrected

--- module_2/lesson_13/src/solution_4.py
+++ module_2/lesson_13/src/solution_4.py
@@ -1,12 +1,12 @@
 from typing import Callable
 
-def type_checker(func: Callable) -> None:
+def type_checker(func: Callable) -> Callable:
     def checker(*args) -> None:
         if len(args) != 2:
             print(f'Ошибка! Количество аргументов не равно двум!')
-        elif type(args[0]) != str:
+        elif not isinstance(args[0], str):
             print(f'Ошибка! Первый аргумент не строка!')
-        elif type(args[1]) != int:
+        elif not isinstance(args[1], int):
             print(f'Ошибка! Второй аргумент не число!')
         else:
             func(*args)
